a97a2f7fbe6bbf65c05bd47c1a0503856f69f95b
Jonas Schmitt <jonas@kreativgebiet.com>
2017-11-30 16:00:52 +0100

    change form

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -37,6 +37,59 @@ $(document).ready(() => {
 
   let customCountryCode = false;
 
+  $('[name="first_name"], [name="middle_name"], [name="last_name"]').on('change', (event) => {
+    if ($(event.currentTarget).val() && $(event.currentTarget).val().match(/\d+/g)) {
+      $(event.currentTarget).css('border-bottom', '1px solid red');
+    } else {
+      $(event.currentTarget).css('border-bottom', '1px solid #eee');
+    }
+  });
+
+  $('[name="password_confirmation"]').on('change', (event) => {
+    if ($(event.currentTarget).val() === $('[name="password"]').val()) {
+      $(event.currentTarget).css('border-bottom', '1px solid #eee');
+      $('[name="password"]').css('border-bottom', '1px solid #eee');
+    } else {
+      $(event.currentTarget).css('border-bottom', '1px solid red');
+      $('[name="password"]').css('border-bottom', '1px solid red');
+    }
+  });
+
+  $('[name="email_confirmation"]').on('change', (event) => {
+    function validateEmail(mail) {
+     if (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(mail)) {
+        return (true)
+      }
+      return (false)
+    }
+
+
+    if (($(event.currentTarget).val() === $('[name="email"]').val()) && validateEmail($(event.currentTarget).val())) {
+      $(event.currentTarget).css('border-bottom', '1px solid #eee');
+      $('[name="email"]').css('border-bottom', '1px solid #eee');
+    } else {
+      $(event.currentTarget).css('border-bottom', '1px solid red');
+      $('[name="email"]').css('border-bottom', '1px solid red');
+    }
+  });
+
+  $('[name="email"]').on('change', (event) => {
+    function validateEmail(mail) {
+     if (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(mail)) {
+        return (true)
+      }
+      return (false)
+    }
+
+    if (($(event.currentTarget).val() === $('[name="email"]').val()) && validateEmail($(event.currentTarget).val())) {
+      $(event.currentTarget).css('border-bottom', '1px solid #eee');
+      $('[name="email"]').css('border-bottom', '1px solid #eee');
+    } else {
+      $(event.currentTarget).css('border-bottom', '1px solid red');
+      $('[name="email"]').css('border-bottom', '1px solid red');
+    }
+  });
+
   $('[name="country_code"]').on('change', (event) => {
     const $target = $(event.currentTarget);
     const value = $target.val();
